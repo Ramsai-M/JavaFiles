83156db06a58ec748e03687421d977b7545baa49
Ramsai-M <ramsaimuthyala97@gmail.com>
2022-07-05 10:37:24 +0530

    Client Basic required details in java

---
+++ ClientDetailsForm.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ClientDetailsForm{
+    public static void main(String[] args){
+        Scanner reader=new Scanner(System.in);
+        System.out.println("Enter a Your Name: ");
+        String name=reader.nextLine();
+        System.out.println("Enter Your age: ");
+        int age=reader.nextInt();
+        System.out.println("Enter Your Gender: ");
+        char gen=reader.next().charAt(0);
+        System.out.println("Enter your Mobile number: ");
+        long mobno=reader.nextLong();
+        System.out.println("Enter the PPA: ");
+        double ppa=reader.nextDouble();
+		System.out.println("Client Name is: "+name+". His Age is "+age+" Years. His gender is "+gen+". \nClient Contact number is +91-"+mobno+". PPA is "+ppa+".");
+    }
+}
