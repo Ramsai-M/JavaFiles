3b5100295c4224506657a9d8b757ba46fd3e6120
Ramsai-M <ramsaimuthyala97@gmail.com>
2022-08-08 18:39:15 +0530

    Prime Number Program using Method in Java

---
+++ PrmeWMtd.java
@@ -0,0 +1,32 @@
+//Prime Number Program using Method in Java
+import java.util.Scanner;
+public class PrmeWMtd{
+    static void checkPrime(long a){
+        long m;
+int flag=0;
+m=a/2;
+if(a==0 || a==1){
+    System.out.println(a+" is not a prime number");
+}else{
+    for(int i=2;i<=m;i++){
+        if(a%i==0){
+            System.out.println(a+" is not a prime number");
+            flag=1;
+            break;
+        }
+        
+    }
+    if(flag==0){
+        System.out.println(a+" is a prime number");
+    }
+}
+    }
+    public static void main(String[] args){
+Scanner reader=new Scanner(System.in);
+System.out.println("Enter a value to check the prime number or not: ");
+long b=reader.nextLong();
+checkPrime(b);
+checkPrime(105);
+checkPrime(229);
+}
+}
